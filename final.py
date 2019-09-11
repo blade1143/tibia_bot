1a17fd108a1731b1989d830904faf79491fad591
blade1143 <blade1143@wp.pl>
2019-09-11 23:43:08 +0200

    final function updated

--- final.py
+++ final.py
@@ -7,8 +7,9 @@ def final_main():
     b = 2280
     # key_bind('F1')
     # key_bind('F2')
-
+    print('glowna funkcja')
     while True:
+        print('wchodzi do while')
         time.sleep(1)
         main()
         '''
